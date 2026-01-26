6ccb09d2f23f56c8847c527075477a64ae4a5346
Ashok Suthar <ashoksuthar1069@gmail.com>
2026-01-26 20:31:55 +0530

    Adding SQL Window Functions Problems

---
+++ 4_SQL_Window_Functions.sql
@@ -0,0 +1,10 @@
+-----Ex.1 Write a query that shows the duration of each ride as a percentage of the total time accrued by riders from each start_terminal.
+
+--Table : tutorial.dc_bikeshare_q1_2012
+
+--- Logic:
+        ---- From Each start terminal calculate total duration in second
+        ---- Divide duration second for each ride by the above total duration in seconds
+
+
+SELECT 
