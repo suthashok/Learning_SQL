90d67a87eba47c8c30cbf87173fe1821365f571f
Ashok Suthar <ashoksuthar1069@gmail.com>
2026-01-27 20:18:55 +0530

    Adding New Problem Statement

--- 4_SQL_Window_Functions.sql
+++ 4_SQL_Window_Functions.sql
@@ -14,3 +14,21 @@ FROM tutorial.dc_bikeshare_q1_2012
 WHERE start_time < '2012-01-08'
 
 
+-----Ex. 2  Write a query that shows a running total of the duration of bike rides grouped by end_terminal,
+-----           and with ride duration sorted in descending order.
+
+SELECT id, duration_seconds, 
+        SUM(duration_seconds) OVER (PARTITION BY end_terminal ORDER BY duration_seconds DESC) AS end_terminal_total,
+        duration_seconds*100.00/start_terminal_total as perc
+FROM tutorial.dc_bikeshare_q1_2012
+WHERE start_time < '2012-01-08'
+
+
+-----Ex. 3 Write a query that shows the 5 longest rides from each starting terminal, ordered by terminal, and longest to shortest rides within each terminal.
+-----            Limit to rides that occurred before Jan. 8, 2012.
+
+--Table : tutorial.dc_bikeshare_q1_2012
+
+--- Logic:
+        ---- 
+        ---- 
