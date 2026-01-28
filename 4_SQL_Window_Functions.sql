5e5bccb3326b403a3f5e08bebbbf5dc9454b5382
Ashok Suthar <ashoksuthar1069@gmail.com>
2026-01-28 14:15:11 +0530

    Adding Percentile Problems

--- 4_SQL_Window_Functions.sql
+++ 4_SQL_Window_Functions.sql
@@ -46,3 +46,13 @@ WHERE start_time < '2012-01-08'
 )a
 WHERE ranks<=5
 ORDER BY duration_seconds DESC
+
+
+-----Ex. 4 Write a query that shows only the duration of the trip 
+-----and the percentile into which that duration falls (across the entire dataset—not partitioned by terminal).
+
+SELECT duration_seconds,NTILE(100) OVER
+         (ORDER BY duration_seconds)
+         AS percentile
+FROM tutorial.dc_bikeshare_q1_2012
+WHERE start_time < '2012-01-08'
